8345c0721e3989406eec0533212dc9a03f28ae97
femi osinowo <fosinowo@governmentcio.com>
2017-02-09 18:28:55 -0500

    adding files

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,29 +1,42 @@
-package example;		
+package example;
 
-import org.openqa.selenium.By;		
-import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
-import org.testng.Assert;		
-import org.testng.annotations.Test;	
-import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;		
-public class NewTest {		
-	    private WebDriver driver;		
-	    
-		@Test				
-		public void testEasy() {	
-			driver.get("http://www.guru99.com/selenium-tutorial.html");  
-			String title = driver.getTitle();				 
-			Assert.assertTrue(title.contains("Free Selenium Tutorials")); 		
-		}	
-		@BeforeTest
-		public void beforeTest() {	
-			//System.setProperty("webdriver.gecko.driver","G:\\Selenium\\Firefox driver\\geckodriver.exe");
-			//WebDriver driver = new FirefoxDriver();
-		    driver = new FirefoxDriver();  
-		}		
-		@AfterTest
-		public void afterTest() {
-			driver.quit();			
-		}		
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import java.util.concurrent.TimeUnit;
+//import org.openqa.selenium.WebElement;
+
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NewTest {
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		// System.setProperty("webdriver.gecko.driver","c:\\geckodriver.exe");
+		// WebDriver driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver", "c:\\webdrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
 }
