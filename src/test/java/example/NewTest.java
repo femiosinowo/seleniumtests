027e7c843179a64ba61fb688617e386ee91171eb
femi osinowo <fosinowo@governmentcio.com>
2017-02-09 23:43:39 -0500

    adding files

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -3,12 +3,11 @@ package example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
-
+import static org.junit.Assert.assertTrue;
 import java.util.concurrent.TimeUnit;
 //import org.openqa.selenium.WebElement;
 
@@ -25,14 +24,42 @@ public class NewTest {
 		Assert.assertTrue(title.contains("Free Selenium Tutorials"));
 	}
 
+	@Test
+	public void htmlsampleTest0() {
+		assertTrue(1 < 2);
+	}
+
+	@Test
+	public void htmlsampleTest1() {
+		assertTrue(1 > 2);
+	}
+
+	@Test
+	public void htmlsampleTest2() {
+		assertTrue(1 < 2);
+	}
+
+	@Test
+	public void htmlsampleTest4() {
+		assertTrue(1 > 2);
+	}
+
 	@BeforeTest
 	public void beforeTest() {
 		// System.setProperty("webdriver.gecko.driver","c:\\geckodriver.exe");
 		// WebDriver driver = new FirefoxDriver();
 
-		// System.setProperty("webdriver.chrome.driver",
-		// "c:\\webdrivers\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "c:\\webdrivers\\chromedriver.exe");
+		 
+
+		String os = System.getProperty("os.name").toLowerCase();
+		System.out.println("Page title is: " + os);
+		if (os.contains("win")) {
+			System.setProperty("webdriver.gecko.driver", "c:\\webdrivers\\chromedriver.exe");
+		} else if (os.contains("nux") || os.contains("nix")) {
+			System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/chromedriver");
+		}
+
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
